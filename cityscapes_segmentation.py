2c653ffb4a8aa6902f197704291d427492313183
jooyeop <royalmansu@gmail.com>
2022-10-04 14:32:38 +0900

    segmentation

--- cityscapes_segmentation.py
+++ cityscapes_segmentation.py
@@ -130,3 +130,84 @@ X_train = np.array(X_train)
 Y_train = np.array(Y_train)
 X_valid = np.array(X_valid)
 Y_valid = np.array(Y_valid)
+
+# Model Creation Function
+def get_unet_model():
+    
+    inputs = tf.keras.layers.Input(shape = [128, 128, 3])
+    
+    #First Downsample
+    f1 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(inputs)
+    b1 = tf.keras.layers.BatchNormalization()(f1)
+    f2 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b1)    # Used later for residual connection
+    
+    m3 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f2)
+    d4 = tf.keras.layers.Dropout(0.2)(m3)
+    
+    # Second Downsample
+    f5 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d4)
+    b5 = tf.keras.layers.BatchNormalization()(f5)
+    f6 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b5)    # Used later for residual connection
+    
+    m7 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f6)
+    d8 = tf.keras.layers.Dropout(0.2)(m7)
+    
+    # Third Downsample
+    f9 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d8)
+    b9 = tf.keras.layers.BatchNormalization()(f9)
+    f10 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b9)    # Used later for residual connection
+    
+    m11 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f10)
+    d12 = tf.keras.layers.Dropout(0.2)(m11)
+    
+    #Forth Downsample
+    f13 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d12)
+    b13 = tf.keras.layers.BatchNormalization()(f13)
+    f14 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b13)    # Used later for residual connection
+    
+    m15 = tf.keras.layers.MaxPooling2D(pool_size = (2, 2), strides = 2)(f14)
+    d16 = tf.keras.layers.Dropout(0.2)(m15)
+    
+    #Fifth Downsample
+    f17 = tf.keras.layers.Conv2D(1024, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(d16)
+    b17 = tf.keras.layers.BatchNormalization()(f17)
+    f18 = tf.keras.layers.Conv2D(1024, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b17)
+
+    
+    # First Upsample
+    m19 = tf.keras.layers.UpSampling2D(size = (2, 2))(f18)
+    d19 = tf.keras.layers.Dropout(0.2)(m19)
+    c20 = tf.keras.layers.Concatenate()([d19, f14])
+    f21 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1 ,activation = "relu")(c20)
+    b21 = tf.keras.layers.BatchNormalization()(f21)
+    f22 = tf.keras.layers.Conv2D(512, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b21)
+    
+    # Second Upsample
+    m23 = tf.keras.layers.UpSampling2D(size = (2, 2))(f22)
+    d23 = tf.keras.layers.Dropout(0.2)(m23)
+    c24 = tf.keras.layers.Concatenate()([d23, f10])
+    f25 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c24)
+    b25 = tf.keras.layers.BatchNormalization()(f25)
+    f26 = tf.keras.layers.Conv2D(256, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b25)
+    
+    # Third Upsample
+    m27 = tf.keras.layers.UpSampling2D(size = (2, 2))(f26)
+    d27 = tf.keras.layers.Dropout(0.2)(m27)
+    c28 = tf.keras.layers.Concatenate()([d27, f6])
+    f29 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c28)
+    b29 = tf.keras.layers.BatchNormalization()(f29)
+    f30 = tf.keras.layers.Conv2D(128, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b29)
+    
+    #Forth Upsample
+    m31 = tf.keras.layers.UpSampling2D(size = (2, 2))(f30)
+    d31 = tf.keras.layers.Dropout(0.2)(m31)
+    c32 = tf.keras.layers.Concatenate()([d31, f2])
+    f33 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(c32)
+    b33 = tf.keras.layers.BatchNormalization()(f33)
+    f34 = tf.keras.layers.Conv2D(64, kernel_size = (3, 3), padding = "same", strides = 1, activation = "relu")(b33)
+    
+    # Output Layer
+    outputs = tf.keras.layers.Conv2D(num_classes, kernel_size = (3, 3), padding = "same", strides = 1, activation = "softmax")(f34)
+    
+    model = tf.keras.Model(inputs = [inputs], outputs = [outputs])
+    return model
